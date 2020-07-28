42ac26fc470b00800113338a088c977db65a56f6
William Cole <ioetbc@gmail.com>
2020-07-28 18:32:27 +0100

    putting keys in env vars

--- src/App.js
+++ src/App.js
@@ -9,20 +9,14 @@ import './App.css';
 class CMS extends Component {
   async componentDidMount() {
     // get file contents from github repo
-    const octokit = new Octokit({ auth: '5d00d0ff68b35b476b45cba8af4e750b930a8fc1' });
+    const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_AUTH });
     const fileContent = await octokit.request('GET /repos/ioetbc/email-trello-cms/contents/email.html', {
       owner: 'ioetbc',
       repo: 'email-trello-cms',
       path: 'email.html',
       type: 'public'
     });
-
-    const buff = new Buffer(fileContent.data.content);
     const emailHtml = Base64.decode(fileContent.data.content)
-
-
-    // console.log('fileContent', emailHtml)
-
 
     // convert html to markdown
     const turndownService = new window.TurndownService();
@@ -31,59 +25,20 @@ class CMS extends Component {
     const JustTheEmailcontent = emailMarkdown.split('Hi there,')[1];
 
     // create trello card
-    const trello = new Trello('c7a88a9d0ae5ab28abbe5527500ec6bd', 'a508fcfdad1e34e2e6b849a65594df307f509d1fb0e379654c26671ffa45beb9');
+    const trello = new Trello(process.env.REACT_APP_TRELLO_APP_KEY, process.env.REACT_APP_TRELLO_TOKEN);
 
     if (emailMarkdown) {
       console.log('trello', trello)
-      // trello.addCard('edit this email', JustTheEmailcontent, '5f2049ef1d997919333e725c',
-      // function (error, trelloCard) {
-      //   if (error) {
-      //       console.log('Could not add card:', error);
-      //       return false;
-      //   }
-      //   else {
-      //       console.log('Added card:', trelloCard);
-      //   }
-      // });
+      trello.addCard('edit this email', JustTheEmailcontent, '5f2049ef1d997919333e725c',
+      function (error, trelloCard) {
+        if (error) {
+            console.log('Could not add card:', error);
+        }
+        else {
+            console.log('Added card:', trelloCard);
+        }
+      });
     }
-
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
-
-var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
-};
-
-trello.TrelloPowerUp.initialize({
-  'board-buttons': function (t, opts) {
-    return [{
-      // we can either provide a button that has a callback function
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'Callback',
-      callback: onBtnClick,
-      condition: 'edit'
-    }, {
-      // or we can also have a button that is just a simple url
-      // clicking it will open a new tab at the provided url
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'URL',
-      condition: 'always',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards' // optional target for above url
-    }];
-  }
-});
-
-
-
-
-
   }
 
   render() {
